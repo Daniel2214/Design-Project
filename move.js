560bdd262aec159bb473304cc426ae0e1bde490f
Daniel2214 <danieljimenez2214@gmail.com>
2015-03-06 22:42:28 -0600

    My first commit

---
+++ move.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){	
+	$("#ad").mouseover(function(){
+			$("#ad").attr("src", "InformaTec2.png");
+		});
+	$("#ad").mouseleave(function(){
+			$("#ad").attr("src", "InformaTec.png");
+		});
+
+});
